b2eae9a0a52d38b8a73ea66aa4edde5aa32efc95
Arthur <arthurramos7890@gmail.com>
2025-02-09 00:50:01 -0300

    por funcoes

--- Calculadora/calc.c
+++ Calculadora/calc.c
@@ -15,15 +15,14 @@
 //Bibliotecas utilizadas
 #include <stdio.h>
 #include <string.h>
-#include "cores.h"
+#include "./libs/cores.h"
 
+void calc(int qtd_cred);
 
-
-int maiorCredito(int credito[], int tamanho); 
 
 int main(){
     char nome[100];
-    int qtd_mat = 0;
+    int qtd_cred, qtd_mats;
 
     printf("====================\n");
     printf("CALCULADORA DE NOTAS\n");
@@ -36,104 +35,37 @@ int main(){
     //Removendo a nova linha
     nome[strcspn(nome, "\n")] = '\0';
 
-    //Input quantidade de materias
-    do{
-        printf("-----------------------------------------------\n");
-        printf("- Quantidade de matérias no semestre:  ");
-        scanf("%d", &qtd_mat);
-    }while(qtd_mat < 1);
-    printf("-----------------------------------------------\n");
+    printf("- Digite a quantidade de matérias: ");
+    scanf("%d", &qtd_mats);
 
-    //array que vai armazenar quantos creditos tem cada materia, [4, 2, 5, 3]
-    int creditos[qtd_mat];
-    float medias[qtd_mat];
-
-    //Pegando do usuario quantos creditos tem cada materia
-    for(int i = 0; i < qtd_mat; i++){
-        printf("- Quantos créditos tem a materia %d: ", i+1);
-        scanf("%d", &creditos[i]);
-    }
-    printf("-----------------------------------------------\n");
-
-    int tam = sizeof(creditos)/sizeof(creditos[0]);
-    int maior_credito = maiorCredito(creditos, tam);
-
-
-    float notas[maior_credito][qtd_mat];
-    float notas_materia[qtd_mat];
-
-
-    //preenchendo a matriz com Zeros para evitar lixo de memória
-    for(int c = 0; c < maior_credito; c++){
-        for(int v = 0; v < qtd_mat; v++){
-            notas[c][v] = 0.0;
-        }
-        //preeenchendo as notas com zero para evitar lixo de memória
-        notas_materia[c] = 0.0;
+    for(int i = 0; i < qtd_mats; i++){
+        printf("- Digite quantos créditos tem a matéria %d: ", i+1);
+        scanf("%d", &qtd_cred);
+        calc(qtd_cred);
     }
 
-    // Ler os valores da matriz coluna por coluna
-    printf("-- INSIRA AS NOTAS --\n");
-    for (int x = 0; x < qtd_mat; x++) {
-        for (int y = 0; y < maior_credito; y++) {
-
-            //Somando as notas
-            if(y < creditos[x]){
-                printf("%d° nota do crédito %d: ", y+1, x+1);
-                scanf("%f", &notas[y][x]);
-                notas_materia[x] += notas[y][x];
-            }else{
-                break;
-            }
-        }
-    }
-
-    float materias_perdidas[qtd_mat];
-
-    //Mostrando o resultado
-    printf("-----------------------------------------------\n");
-
-    for(int d = 0; d < qtd_mat; d++){
-        float media = notas_materia[d] / creditos[d];
-
-        if(media >= 7.0){
-            printf("Materia %d <-> " BG_WHITE GREEN "Média %.1f" RESET"\n", d+1, media);
-            materias_perdidas[d] = 7.1;
-        }else{
-            printf("Materia %d <-> " BG_WHITE RED "Média %.1f" RESET "\n", d+1, media);
-            materias_perdidas[d] = media;
-        }
-    }
-
-    //--------------------------------------------------------------------//
-
-    float final = 0.0;
-
-    //erro aqui!!
-    printf("-----------------------------------------------\n");
-    printf("-- Boletim %s --\n", nome);
-    printf("-----------------------------------------------\n");
-    for(int count = 0; count < qtd_mat; count++){
-        if(materias_perdidas[count] < 7.0){
-            final = (5 - (materias_perdidas[count] * 0.6)) / 0.4;
-            printf(BG_RED " " RESET " Vi que você perdeu na Matéria %d | Você precisa tirar " BG_WHITE BLACK "%.1f" RESET " na final para passar!!\n", count+1, final);
-        }else{
-            printf(BG_GREEN " " RESET " Vi que você passou na Matéria %d, meus parabêns!!\n", count+1);
-        }
-    }
-
-    return 0;
 }
 
-//Função que retorna qual o maior credito para criar a matriz de notas
-int maiorCredito(int credito[], int tamanho) {
-    int maior = credito[0];
+void calc(int qtd_cred){
+    float notas[qtd_cred];
+    float media = 0;
 
-    for (int i = 1; i < tamanho; i++) {
-        if (credito[i] > maior) {
-            maior = credito[i];
-        }
+    for(int i = 0; i < qtd_cred; i++){
+        printf("-- Nota %d: ", i+1);
+        scanf("%f", &notas[i]);
+        media += notas[i];
     }
+    printf("\n");
 
-    return maior;
+    media /= qtd_cred;
+
+    float final = ((5 - (media * 0.6)) / 0.4);
+
+    if(media > 6.99){
+        printf( GREEN "Média %.2f - " RESET, media);
+        printf("Parabéns, você passou na matéria!!\n");
+    }else{
+        printf( RED "Média %.2f - " RESET, media);
+        printf("Vi que você perder na matéria e precisa de %.2f na final!!\n", final);
+    }    
 }
